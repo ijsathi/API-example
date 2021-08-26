f3405c729477c15dbde16b7c49364e24e0a57bdc
ijsathi <ijsathi2001rs@gmail.com>
2021-08-27 01:24:43 +0600

    kanye & buddy

---
+++ buddy.js
@@ -0,0 +1,27 @@
+const loadBuddy = () =>{
+    fetch('https://randomuser.me/api/?results=5')
+    .then(res => res.json())
+    .then(data => displayBuddy(data))
+
+}
+
+loadBuddy();
+ 
+const displayBuddy = data =>{
+    const buddies = data.results;
+    const buddyDiv = document.getElementById('buddies');
+    for (const buddy of buddies){
+        const p = document.createElement('p');
+        p.innerText =`Name: ${buddy.name.first} ${buddy.name.last}, Email: ${buddy.email}
+        `;
+        buddyDiv.appendChild(p)
+    }
+
+}
+
+
+
+
+
+
+
---
+++ kanye.js
@@ -0,0 +1,9 @@
+function loadQuotes(){
+    fetch('https://api.kanye.rest/')
+    .then(res => res.json())
+    .then(data => displayQuotes(data))
+}
+const displayQuotes = quote =>{
+    const quoteElement = document.getElementById('quote')
+    quoteElement.innerText = quote.quote
+}
