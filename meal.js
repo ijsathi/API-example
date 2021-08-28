1bc993fe5d3046c9c7dda9464f131994ade40ea9
ijsathi <ijsathi2001rs@gmail.com>
2021-08-28 11:34:00 +0600

    meal

--- meal.js
+++ meal.js
@@ -1,7 +1,7 @@
 const searchFood = ()=>{
     const searchFeild = document.getElementById('search-feild')
     const searchText = searchFeild.value;
-    // console.log(searchText);
+    console.log(searchText);
     searchFeild.value = '';
     const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
     fetch(url)
@@ -11,7 +11,7 @@ const searchFood = ()=>{
 const displaySearchResult = meals =>{
     const searchResult = document.getElementById('search-result')
     searchResult.innerHTML = '';
-
+    
     meals.forEach(meal =>{
         const div = document.createElement('div')
         div.classList.add('col')
@@ -28,15 +28,19 @@ const displaySearchResult = meals =>{
     })
 }
 
-const loadMealDetail = mealId =>{
+const loadMealDetail =async mealId =>{
     const url =`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displayMealDetails(data.meals[0]))
+
+    const res = await fetch(url);
+    const data = await res.json();
+    displayMealDetails(data.meals[0])
+    // fetch(url)
+    // .then(res => res.json())
+    // .then(data => displayMealDetails(data.meals[0]))
 }
 
 const displayMealDetails = meal =>{
-    console.log(meal);
+    // console.log(meal);
     const mealDetails = document.getElementById('meal-details')
     mealDetails.textContent =''
     const div = document.createElement('div')
---
+++ quiz.js
@@ -0,0 +1,14 @@
+// const data={ result: [ {userName: {title:'Mr.',name:"sakib khan"} } ] }
+// const dt= data.result[0].userName.name
+// console.log(dt);
+// const p=[1,2,3]; 
+// const q=p.find
+// __(  n=> Math.pow(n, 3));
+
+
+// const bondCode=` I am Fake James bond . My new code is: 00${7+1+2}`
+// console.log(bondCode);
+// const url =`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+// fetch(url)
+// .then(res=>res.json())
+// .then(data => console.log(data).try?__(error => console.log(error))
